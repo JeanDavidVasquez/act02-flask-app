b8a0359a8caf416137fe2a59da69e05b0741b543
¨jeandavidvasquez¨ <jeandavidvasquezyaruqui@gmail.com>
2025-04-16 09:49:11 -0500

    url

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    url = https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt
+    url = "https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt"
     response = requests.get(url)
     contenido = response.text.strip().splitlines()
 
